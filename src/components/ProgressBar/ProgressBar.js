d61be294bfc17121ee804a1fd4ffd73cf7b0469a
Unbound <unbound9dev@gmail.com>
2021-10-06 13:12:20 +0300

    finished progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,56 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from '../../constants'
+import VisuallyHidden from '../VisuallyHidden'
 
+const SIZES_STYLE = {
+  small: {
+    '--bar-height': '8px',
+    '--border-radius': '4px',
+  },
+  medium: {
+    '--bar-height': '12px',
+    '--border-radius': '4px',
+  },
+  large: {
+    '--bar-height': '16px',
+    '--padding': '4px',
+    '--border-radius': '8px',
+  },
+}
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
-};
+  const styles = SIZES_STYLE[size]
+  return (
+    <Wrapper
+      role='progressbar'
+      aria-valuenow={value}
+      value={value}
+      aria-valuemin='0'
+      aria-valuemax='100'
+      max='100'
+      style={styles}
+    >
+      <InnerBar value={value}>{value}</InnerBar>
+    </Wrapper>
+  )
+}
 
-export default ProgressBar;
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  padding: var(--padding);
+  border-radius: var(--border-radius);
+`
+
+const InnerBar = styled.div`
+  height: var(--bar-height);
+  width: ${(p) => p.value + '%'};
+  background-color: ${COLORS.primary};
+  color: transparent;
+  border-radius: 4px;
+  border-top-right-radius: ${(p) => (p.value > 98 ? '4px' : '0px')};
+  border-bottom-right-radius: ${(p) => (p.value > 98 ? '4px' : '0px')};
+`
+
+export default ProgressBar
